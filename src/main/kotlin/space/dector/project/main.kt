ac8470d33254e402abb204df24008869f073c179
Denys M <dector@dector.space>
2021-01-19 23:11:16 +0400

    Add initial models

--- src/main/kotlin/space/dector/project/main.kt
+++ src/main/kotlin/space/dector/project/main.kt
@@ -1,5 +1,28 @@
 package space.dector.project
 
+
 fun main() {
     println("It works!")
+}
+
+interface ByteArray {
+    val size: Int
+}
+
+class FixedByteArray(
+    size: Int
+) : ByteArray {
+
+    // TODO
+
+    override val size = size
+}
+
+class DynamicByteArray(
+    initialSize: Int
+) : ByteArray {
+
+    // TODO
+
+    override val size = initialSize
 }
